81ccfa03f0eb9281d8af3a58f8e0b199a1b6c73e
lokosmotos <danial.khalif.dk@gmail.com>
2025-05-03 17:18:20 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,10 @@
+# backend/app.py
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "🐾 Purrfect HR Backend Running"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
