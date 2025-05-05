40dc1c4dfa9fe1925d0ef4e26c0176a2764febe8
lokosmotos <danial.khalif.dk@gmail.com>
2025-05-05 21:34:52 +0800

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,12 @@
-
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4F46E5', // Indigo
+        secondary: '#10B981', // Emerald
+      },
+    },
+  },
+  plugins: [],
+}
