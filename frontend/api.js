1d046805795311c84ab320c2ce7040237f3a56a5
lokosmotos <danial.khalif.dk@gmail.com>
2025-05-07 21:18:43 +0800

    Delete frontend directory

--- frontend/api.js
+++
@@ -1,9 +0,0 @@
-// src/services/api.js
-import axios from 'axios';
-
-const API = axios.create({
-  baseURL: 'https://daniexc.onrender.com',
-});
-
-export const getCandidates = () => API.get('/candidates');
-export const addCandidate = (data) => API.post('/candidates', data);
